8eed7aee1f02c10c66601168f11df511cb9f20a5
Astrobot91 <nehalpal41.np@gmail.com>
2022-02-15 21:48:15 +0530

    Modified adddigits.c

--- adddigits.c
+++ adddigits.c
@@ -5,17 +5,16 @@
 
 int main(void)
 {
-    int n, count = 0, sum = 0; 
-    int i = 1, p;
+    int n, sum = 0;
 
     printf("Enter the number: ");
     scanf("%d", &n);
 
-    while (n>0)
+    while (n > 0)
     {
-        sum+=n%10;
-	n=n/10;
+        sum += n % 10;
+        n = n / 10;
     }
 
-    printf("%d\n", sum);
+    printf("\nTotal sum of all digits = %d\n", sum);
 }
